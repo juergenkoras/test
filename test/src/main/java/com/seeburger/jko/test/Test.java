3994b0dd9532d9c83a290465e3d2a7206b78cc47
Juergen Koras <j.koras@seeburger.de>
2017-01-11 15:59:57 +0100

    initial

---
+++ test/src/main/java/com/seeburger/jko/test/Test.java
@@ -0,0 +1,17 @@
+/*
+ * Test.java
+ *
+ * created at 11.01.2017 by koras <YOURMAILADDRESS>
+ *
+ * Copyright (c) SEEBURGER AG, Germany. All Rights Reserved.
+ */
+package com.seeburger.jko.test;
+
+
+public class Test
+{
+
+}
+
+
+
